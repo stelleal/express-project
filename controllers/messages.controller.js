b289da036025ac9e4227ee9efd0e97f0c2dbcfb5
stelleal <stellalleal@gmail.com>
2022-05-09 21:58:02 -0300

    Using templating engines - Handlebars

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 function getMessages(req, res) {
-	res.sendFile(path.join(__dirname, '..', 'public', 'images', 'fish.jpg'));
+	res.render('messages', {
+		title: 'Messages to my Friends',
+		friend: 'Midori',
+	});
+	// res.sendFile(path.join(__dirname, '..', 'public', 'images', 'fish.jpg'));
 }
 
 function postMessage(req, res) {
